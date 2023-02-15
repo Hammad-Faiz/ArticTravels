edbb1241d11556279d1711a08841d0f46a82af5c
Hammad Faiz <hammadfaiz172@gmail.com>
2023-02-15 12:06:58 -0500

    implemented headlines

--- src/components/PlanTrips.js
+++ src/components/PlanTrips.js
@@ -2,12 +2,21 @@ import './styles/PlanTrips.css'
 
 function PlanTrips(){
   return (
-    <div className='planning'>
-        <div>Testing</div>
-        <div>Testing</div>
-        <div>Testing</div>
-        <div>Testing</div>
-        <div>Testing</div>
+    <div className='mainPage'>
+        <div className='plan'>
+          <div className='headline'>
+            <i>Plan The Perfect Winter Trip</i>
+          </div>
+          <div className='headline2'>
+            <p>Easily plan your ideal ski trip from home</p>
+          </div>
+          <div>
+            <div>
+              Region
+            </div>
+            
+          </div>
+        </div>
     </div>
   )
 }
