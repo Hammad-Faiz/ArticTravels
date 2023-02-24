d019fd8ca663048e5997b616e02c5ee0ea4f86e8
Hammad Faiz <hammadfaiz172@gmail.com>
2023-02-24 13:16:24 -0500

    deleted most of the planning section for home page
    thinking of using matrial ui instead

--- src/components/PlanTrips.js
+++ src/components/PlanTrips.js
@@ -4,17 +4,7 @@ function PlanTrips(){
   return (
     <div className='mainPage'>
         <div className='plan'>
-          <div className='headline'>
-            <i>Plan The Perfect Winter Trip</i>
-          </div>
-          <div className='headline2'>
-            <p>Easily plan your ideal ski trip from home</p>
-          </div>
-          <div>
-            <div>
-              Region
-            </div>
-          </div>
+          
         </div>
     </div>
   )
